aca43a9d4641350b6678eacf16d69587d877d25b
Jiapeng Wu <49953745+JackWoo0831@users.noreply.github.com>
2022-05-13 12:00:13 +0800

    Add files via upload
    update the code of generating labels for MOT17 train dataset

--- generate_labels_for_MOT17.py
+++ generate_labels_for_MOT17.py
@@ -19,10 +19,17 @@ image_wh_dict = {
     'MOT17-10-SDP': (1920, 1080),
     'MOT17-11-SDP': (1920, 1080),
     'MOT17-13-SDP': (1920, 1080),
+    'MOT17-01-SDP': (1920, 1080),
+    'MOT17-03-SDP': (1920, 1080),
+    'MOT17-06-SDP': (640, 480),
+    'MOT17-07-SDP': (1920, 1080),
+    'MOT17-08-SDP': (1920, 1080),
+    'MOT17-12-SDP': (1920, 1080),
+    'MOT17-14-SDP': (1920, 1080),
 }  # seq->(w,h) 字典 用于归一化
 
 
-def generate_labels(split='train', if_certain_seqs=False):
+def generate_labels(split='train', if_certain_seqs=False, if_write_txt=True):
     """
     split: str, 'train', 'val' or 'test'
     if_certain_seqs: bool, use for debug. 
@@ -40,6 +47,9 @@ def generate_labels(split='train', if_certain_seqs=False):
     # 每张图片分配一个txt
     # 要从sequence的txt里分出来
 
+    current_id = 0  # 表示写入txt的当前id 一个目标在所有视频序列里是唯一的
+    last_id = -1  # 表示写入的上一个id
+
     for seq in seq_list:
         seq_dir = osp.join(DATA_ROOT, split, seq, 'gt', 'gt.txt')
         with open(seq_dir, 'r') as f:
@@ -50,6 +60,10 @@ def generate_labels(split='train', if_certain_seqs=False):
                 # 需要写进新文件行的文字
                 # 例如 0 1 781 262 200 631  (0 id x_c y_c w h)
                 frame = current_line[0]  # 第几帧
+                id_in_frame = int(current_line[1])  # 当前帧当前目标的id
+                if not id_in_frame == last_id:  # 如果到了下一个id
+                    current_id += 1  # 写入文件的id加一
+                    last_id = id_in_frame  # 更新last id
                 # 写到对应图片的txt
 
                 # to_file = osp.join(DATA_ROOT, 'labels_with_ids', split, seq, 'img1', frame.zfill(6) + '.txt')
@@ -70,12 +84,23 @@ def generate_labels(split='train', if_certain_seqs=False):
                     w, h = w / image_w, h / image_h
                     x_c, y_c = x_c / image_w, y_c / image_h
 
-                    write_line = '0 ' + id + ' ' + str(x_c) + ' ' \
-                                 + str(y_c) + ' ' + str(w) + ' ' \
-                                 + str(h) + '\n'
+                    write_line = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
+                        current_id, x_c, y_c, w, h)
 
                     f_to.write(write_line)
                 f_to.close()
+        f.close()
+
+    print(f"writing txt{if_write_txt}")
+    if if_write_txt:
+        with open('./mot.txt', 'a') as f:
+            for seq in seq_list:
+                img_dir = osp.join(DATA_ROOT, split, seq)  # 该序列下所有图片路径
+                imgs = sorted(os.listdir(img_dir))  # 所有图片
+
+                for img in imgs:  # 遍历该序列下的图片
+                    f.write('MOT17/' + 'images/' + split + '/' + seq + '/' \
+                             + 'img1/' + img + '\n')
         f.close()
 
 
@@ -83,9 +108,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--split', type=str, default='train', help='train or test')
     parser.add_argument('--if_certain_seqs', type=bool, default=False, help='for debug')
+    parser.add_argument('--if_write_txt', type=bool, default=True, help='write mot17.train or mot17.val')
 
     opt = parser.parse_args()
 
-    generate_labels(opt.split, opt.if_certain_seqs)
+    generate_labels(opt.split, opt.if_certain_seqs, opt.if_write_txt)
 
     print('Done!')
